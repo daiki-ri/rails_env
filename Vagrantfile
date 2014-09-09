c6c0c7ccb6e5318ee06c69848926d2c2c1ff4783
daiki-rikitake <daiki-rikitake@altplus.co.jp>
2014-09-09 18:40:00 +0900

    added recipe rails
     とりあえず、railsをインストールするレシピ追加。

--- Vagrantfile
+++ Vagrantfile
@@ -118,6 +118,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
         recipe[nodejs]
         recipe[mysql]
         recipe[dstat]
+        recipe[rails]
     ]
   end
 
---
+++ chef-repo/site-cookbooks/rails/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: rails
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+gem_package "rails" do
+    action :install
+end
